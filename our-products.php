ad2793ad663cbaca605c5ec36a74490ce6721cee
AlexanderDryapko <alexander.driapko@gmail.com>
2018-10-03 14:56:34 +0300

    Integration our products page to WordPress

--- our-products.php
+++ our-products.php
@@ -5,17 +5,17 @@ Template Name: Products
 get_header(); ?>
 <main>
 	<div class="top-baner bg-img support-baner">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/our-project-bg.png" alt="">
+		<img src="<?php echo get_field('p_top_image_background'); ?>" alt="<?php echo get_field('p_top_page_title'); ?>">
 		<div class="container">
 			<div class="heading-box">
-				<h1 class="h1">Products and Services</h1>
+				<h1 class="h1"><?php echo get_field('p_top_page_title'); ?></h1>
 			</div>
 		</div>
 	</div>
 	<div class="text-block">
 		<div class="text-container tac">
-			<h2 class="h2">Our Products</h2>
-			<p>Working collaboratively with our clients and partners, Logos Technologies provides innovative solutions to solve problems of national and international significance. We are always seeking new and effective ways to develop and prove ideas and create successful, best-fit solutions for our clients. We offer a broad range of system design, development, integration, manufacturing, and operation and intelligence services.</p>
+			<h2 class="h2"><?php echo get_field('p_products_section_title'); ?></h2>
+			<p><?php echo get_field('p_products_section_subtitle') ?></p>
 		</div>
 	</div>
 	<div class="products-holder">
@@ -27,159 +27,66 @@ get_header(); ?>
 				<div class="hr-box"><div class="hr"></div></div>
 			</div>
 			<ul class="product-list no-slider">
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/MMSP.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">MMSP</h2>
-									<p>Platform Agnostic Multi-Sensor Pod</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/Serenity.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">Serenity</h2>
-									<p>Aerostat or mast mounted dual fire detection system</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/Redkite-I.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">Redkite-I</h2>
-									<p>Powerful Wide-Area Motion Imagery in a Small Group 3 UAS</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/Redkite.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">Redkite</h2>
-									<p>Lightweight Wide-Area Motion Imagery System for Aircraft</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/Kestrel-Block-II.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">Kestrel block II</h2>
-									<p>Lightweight day and night aerostat mounted WAMI system</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/Simera.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">Simera</h2>
-									<p>Lightweight aerostat mounted wide-area motion system</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="box">
-						<a href="">
-							<span class="flip-box-front">
-								<div class="img"><img src="<?php echo get_template_directory_uri(); ?>/images/SilentHawk.png" alt=""></div>
-								<div class="product-info">
-									<h2 class="product-name">SilentHawk</h2>
-									<p>Hybrid-Electric Military Motorcycle</p>
-								</div>
-							</span>
-						</a>
-					</div>
-				</li>
+
+				<?php 
+				$posts = get_field('p_products_list');
+
+				if( $posts ): ?>
+
+					<?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+					    <li>
+							<div class="box">
+								<a href="<?php echo get_permalink( $p->ID ); ?>">
+									<span class="flip-box-front">
+										<div class="img"><img src="<?php echo get_the_post_thumbnail_url( $p->ID ); ?>" alt=""></div>
+										<div class="product-info">
+											<h2 class="product-name"><?php echo get_the_title( $p->ID ); ?></h2>
+											<p><?php echo get_the_excerpt( $p->ID ); ?></p>
+										</div>
+									</span>
+								</a>
+							</div>
+						</li>
+					<?php endforeach; ?>
+				<?php endif; ?>
+				
 			</ul>
 		</div>
 	</div>
 	<div class="our-services products-page">
 		<div class="container">
 			<div class="text-container tac">
-				<h2 class="h2">Our Services</h2>
-				<p>Working collaboratively with our clients and partners, Logos Technologies provides innovative solutions to solve problems of national and international significance. We are always seeking new and effective ways to develop and prove ideas, and create successful, best-fit solutions for our clients. We offer a broad range of system design, development, integration, manufacturing, and operation and intelligence services.</p>
+				<h2 class="h2"><?php echo get_field('p_our_services_title') ?></h2>
+				<p><?php echo get_field('p_our_services_subtitle') ?></p>
 			</div>
 			<div class="services-list row">
-				<div class="col-md-3">
-					<div class="services-list-item">
-						<a href="">
-							<span class="img bg-img">
-								<img src="<?php echo get_template_directory_uri(); ?>/images/services1.png" alt="">
-							</span>
-							<span class="description clearfix">
-								<h3 class="services-name h4 red-text">LARGE DATA PROCESSING AND STORAGE</h3>
-								<span class="default-btn">LEARN</span>
-							</span>
-						</a>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="services-list-item">
-						<a href="">
-							<span class="img bg-img">
-								<img src="<?php echo get_template_directory_uri(); ?>/images/services2.png" alt="">
-							</span>
-							<span class="description clearfix">
-								<h3 class="services-name h4 red-text">ADVANCED ANALYTICS</h3>
-								<span class="default-btn">INQUIRE</span>
-							</span>
-						</a>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="services-list-item">
-						<a href="">
-							<span class="img bg-img">
-								<img src="<?php echo get_template_directory_uri(); ?>/images/services3.png" alt="">
-							</span>
-							<span class="description clearfix">
-								<h3 class="services-name h4 red-text">OPERATIONS SUPPORT FOR SENSOR SYSTEMS</h3>
-								<span class="default-btn">EXPLORE</span>
-							</span>
-						</a>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="services-list-item">
-						<a href="">
-							<span class="img bg-img">
-								<img src="<?php echo get_template_directory_uri(); ?>/images/services4.png" alt="">
-							</span>
-							<span class="description clearfix">
-								<h3 class="services-name h4 red-text">R&D LIFECYCLE SERVICES</h3>
-								<span class="default-btn">VISUALIZE</span>
-							</span>
-						</a>
-					</div>
-				</div>
+
+				<?php 
+
+				$posts = get_field('p_our_services_list');
+
+				if( $posts ): ?>
+
+					<?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+
+								<div class="col-md-3">
+									<div class="services-list-item">
+										<a href="<?php echo get_permalink( $p->ID ); ?>">
+											<span class="img bg-img">
+												<img src="<?php echo get_the_post_thumbnail_url( $p->ID ); ?>" alt="">
+											</span>
+											<span class="description clearfix">
+												<h3 style="text-transform: uppercase;" class="services-name h4 red-text"><?php echo get_the_title( $p->ID ); ?></h3>
+												<span class="default-btn"><?php the_field('s_link_label', $p->ID); ?></span>
+											</span>
+										</a>
+									</div>
+								</div>
+
+					<?php endforeach; ?>
+
+				<?php endif; ?>
+
 			</div>
 		</div>
 	</div>
