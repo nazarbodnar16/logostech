096cf9b152b40115943a362036ee67c5600cde5b
PERFECTIAL1\nbodnar <nazar.bodnar@perfectial.com>
2018-10-10 13:38:31 +0300

    last changes

--- careers.php
+++ careers.php
@@ -27,7 +27,7 @@ get_header(); ?>
     </div>
     <div class="container">
       <div class="container">
-        <div class="expeertise-details">
+        <div class="expeertise-details career-detail">
           <h3 class="h3 ttu">What We Do</h3>
           <p>We are a dynamic engineering and technology company that specializes in sensors, advanced analytics and large, multisource datasets. We do everything from R&D and production to system integration, to in-field analysis and exercise support.</p>
         </div>
@@ -40,7 +40,7 @@ get_header(); ?>
     </div>
     <div class="container">
       <div class="container">
-        <div class="expeertise-details">
+        <div class="expeertise-details career-detail">
           <h3 class="h3 ttu">What We Are Looking For</h3>
           <p>We want smart, creative problem solvers with fresh ideas on how to create faster, more efficient technologies—people who can work well in small, nimble teams, and who are hands-on and ready to travel to support customers in the field.</p>
         </div>
@@ -53,7 +53,7 @@ get_header(); ?>
     </div>
     <div class="container">
       <div class="container">
-        <div class="expeertise-details">
+        <div class="expeertise-details career-detail">
           <h3 class="h3 ttu">Benefits</h3>
           <p>For those joining our company, Logos provides an unparalleled opportunity to develop and work with cutting-edge systems—real systems, not just computer models—and test them under real-world conditions. We also offer a great healthcare package (dental, vision, prescription drug, short and long term disability insurance, and life insurance, etc.) as well as flexible spending accounts and paid time off.</p>
         </div>
